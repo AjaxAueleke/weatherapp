723c512d98b324297c18cf0177db3224c4573d6b
Ahmed <ahmed.jamil7410@gmail.com>
2021-09-20 18:11:08 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {Navbar,Container, Button,InputGroup, FormControl} from 'react-bootstrap';
 import Temperature from "./components/Temperature";
 const API_KEY = "bdcb0085e5ff638cc0f920d918a5ddf6";
 
 function App(props) {
+  const [name, setName] = useState('');
   const [data, setData] = useState({});
   const [location, setLocation] = useState({});
   if (data && Object.keys(data).length === 0 && data.constructor === Object) {
@@ -22,34 +24,46 @@ function App(props) {
     async function getWeatherData() {
       console.log("USE EFFECT IS RUNNING");
       try {
-        const response = await axios.get(
+        let response = await axios.get(
           `http://api.openweathermap.org/data/2.5/weather?lat=${Math.round(
             data.lat
           )}&lon=${Math.round(data.long)}&appid=${API_KEY}&units=metric`
         );
+        if (name) {
+          response = await axios.get(``) 
+        }
         console.log(response.data);
-        setLocation(response.data || {});
+        setLocation(response.data);
       } catch (err) {
         console.error(err);
       }
     }
-    getWeatherData(); // eslint-disable-next-line
-  }, [data]);
+    getWeatherData();
+  }, [data,name]);
+
   return (
     <div>
-      <video autoPlay="autoPlay" loop muted = "muted">
-        <source src="rain.mp4" type="video/mp4" />
-      </video>
-      <div className="container">
-        <Temperature
-          city={location.name || "Karachi"}
-          temp={location.main ? location.main.temp : "39"}
-          description = {"Rain"}
-          // description={location.weather ? location.weather[0].main : "Rainy"}
-          high={location.main ? location.main.temp_max : "145"}
-          low={location.main ? location.main.temp_min : "39"}
-        />
-      </div>
+      <Navbar sticky="top" className="nav"expand="lg" variant="dark" bg="dark">
+        <Container>
+          <Navbar.Brand href="#">Weather App</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <InputGroup className="mb-3">
+    <FormControl
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+      onChange = {(e) => (setName(e.target.value))}
+    />
+  </InputGroup>
+      <Temperature
+        city={location.name || "Karachi"}
+        temp={location.main ? location.main.temp : "39"}
+        description={"Rain"}
+        // description={location.weather ? location.weather[0].main : "Rainy"}
+        high={location.main ? location.main.temp_max : "145"}
+        low={location.main ? location.main.temp_min : "39"}
+      />
     </div>
   );
 }
--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "./Temperature.css";
+import { Card  } from "react-bootstrap";
 
 const Temperature = (props) => {
   return (
-    <div className ="temperature">
-      <h1 className="city">{props.city}</h1>
-      <h4 className="main">{props.description.toUpperCase()}</h4>
-      <h1 className="main-temp">{Math.round(props.temp)}</h1>
-      <h4 class="high-low">
-        <span>H:{Math.round(props.high)}</span>
-        <span>L:{Math.round(props.low)}</span>
-      </h4>
-    </div>
+    <Card className="m-auto text-center d-block w-lg-50 w-50">
+      <Card.Header>{props.city}</Card.Header>
+      <Card.Body>
+        <Card.Title>{Math.round(props.temp)}</Card.Title>
+        <Card.Text>{props.description.toUpperCase()}</Card.Text>
+        <h4 class="high-low">
+          <span>H:{Math.round(props.high)}</span>
+          <span>L:{Math.round(props.low)}</span>
+        </h4>
+      </Card.Body>
+    </Card>
   );
 };
 
